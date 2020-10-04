a9131eadb668616e01a0c64a0ba9b7a113a62a29
tantd <tantd@vng.com.vn>
2020-10-04 11:03:37 +0700

    Initial Commit

---
+++ DemoRecordView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoRecordView
+//
+//  Created by Tấn Tạ Đình on 10/4/20.
+//  Copyright © 2020 com.ta.vn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
