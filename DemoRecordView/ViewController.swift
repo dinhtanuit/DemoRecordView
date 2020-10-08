e0e5457eb9d62f912b865a1e89b565b31563de3f
tantd <tantd@vng.com.vn>
2020-10-08 19:12:46 +0700

    save video

--- DemoRecordView/ViewController.swift
+++ DemoRecordView/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
     @IBOutlet weak var recordView: UIView!
@@ -21,21 +22,73 @@ class ViewController: UIViewController {
 //        self.isStartRecord = true
         
         // Do any additional setup after loading the view.
+        self.btnRecord.setTitle("Start Record", for: .normal)
+        
     }
 
     @IBAction func actionRecord(_ sender: Any) {
         self.isStartRecord = !self.isStartRecord
-        if self.isStartRecord {
-            self.glimpse.startRecording(self.recordView) { (url) in
-                //
-            }
-            
-//            self.recorder.start()
-        } else {
-//            self.recorder.stop()
-        }
         
+        if self.isStartRecord {
+            self.btnRecord.setTitle("Recording", for: .normal)
+
+            glimpse.startRecording(self.recordView, onCompletion: { [weak self] videoUrl in
+                
+                guard let strongSelf = self else {
+                    return
+                }
+                
+                if let url = videoUrl {
+                    strongSelf.saveVideoToAlbum(url) { (error) in
+                        if error == nil {
+                            DispatchQueue.main.async {
+                                let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
+                                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                                alertController.addAction(defaultAction)
+                                strongSelf.present(alertController, animated: true, completion: nil)
+                            }
+                        }
+                    }
+                }
+            })
+        } else {
+            self.btnRecord.setTitle("Start Record", for: .normal)
+
+            glimpse.stop()
+        }
     }
+    
+    func requestAuthorization(completion: @escaping ()->Void) {
+            if PHPhotoLibrary.authorizationStatus() == .notDetermined {
+                PHPhotoLibrary.requestAuthorization { (status) in
+                    DispatchQueue.main.async {
+                        completion()
+                    }
+                }
+            } else if PHPhotoLibrary.authorizationStatus() == .authorized{
+                completion()
+            }
+        }
+
+
+
+    func saveVideoToAlbum(_ outputURL: URL, _ completion: ((Error?) -> Void)?) {
+            requestAuthorization {
+                PHPhotoLibrary.shared().performChanges({
+                    let request = PHAssetCreationRequest.forAsset()
+                    request.addResource(with: .video, fileURL: outputURL, options: nil)
+                }) { (result, error) in
+                    DispatchQueue.main.async {
+                        if let error = error {
+                            print(error.localizedDescription)
+                        } else {
+                            print("Saved successfully")
+                        }
+                        completion?(error)
+                    }
+                }
+            }
+        }
     
 }
 
